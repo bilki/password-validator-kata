0df6401cd6b474c2ac078cc0f2238fc4c05ac0a7
Roberto Serrano <r.semarcos@gmail.com>
2022-07-22 16:37:27 +0200

    Red - Add first property, password with more than 8 chars

--- src/test/scala/com/lambdarat/Generators.scala
+++ src/test/scala/com/lambdarat/Generators.scala
@@ -1,5 +1,14 @@
 package com.lambdarat
 
 import org.scalacheck.Gen
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
 
-object Generators {}
+object Generators {
+
+  lazy val greaterThanEightCharsGen: Gen[String] =
+    for {
+      n    <- Gen.choose(9, 100)
+      head <- Gen.listOfN(n, arbitrary[Char])
+    } yield head.mkString
+  }
--- src/test/scala/com/lambdarat/PasswordValidatorSpec.scala
+++ src/test/scala/com/lambdarat/PasswordValidatorSpec.scala
@@ -2,12 +2,26 @@ package com.lambdarat
 
 import munit.{FunSuite, ScalaCheckSuite}
 import org.scalacheck.Prop.{forAll, forAllNoShrink}
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
 
 class PasswordValidatorSpec extends FunSuite with ScalaCheckSuite {
 
-  test("Property test") {
-    forAll { number: Int =>
-      assertEquals(-(-(number)), number)
+/*
+Have more than 8 characters
+Contains a capital letter
+Contains a lowercase
+Contains a number
+Contains an underscore
+*/
+
+  test("All passwords with more than 8 characters must be valid") {
+    forAll(Generators.greaterThanEightCharsGen) { password =>
+      val result = PasswordValidator.validatePassword(password)
+
+      val expected = true
+
+      assertEquals(result, expected)
     }
   }
 
