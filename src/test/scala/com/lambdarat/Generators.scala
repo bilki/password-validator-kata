7f6970ed83902faab74a3f0f48f4b3984c10564c
Roberto Serrano <r.semarcos@gmail.com>
2022-07-22 17:29:45 +0200

    Attempt to create a valid password generator

--- src/test/scala/com/lambdarat/Generators.scala
+++ src/test/scala/com/lambdarat/Generators.scala
@@ -8,7 +8,19 @@ object Generators {
 
   def strAtLeastNGen(size: Int, limit: Int): Gen[String] =
     for {
-      n    <- Gen.choose(size, limit)
-      head <- Gen.listOfN(n, arbitrary[Char])
-    } yield head.mkString
-  }
+      n   <- Gen.choose(size, limit)
+      str <- Gen.listOfN(n, arbitrary[Char])
+    } yield str.mkString
+
+  lazy val validPasswordGen: Gen[String] =
+    for {
+      input         <- strAtLeastNGen(9, 100)
+      idxs          <- Gen.pick(4, 0 until input.size)
+      capitalLetter <- Gen.alphaUpperChar
+      lowerLetter   <- Gen.alphaLowerChar
+      number        <- Gen.numChar
+      replaceChar = idxs.zip(List(capitalLetter, lowerLetter, number, '_'))
+    } yield replaceChar.foldLeft(input) { case (acc, (idx, char)) =>
+      acc.updated(idx, char)
+    }
+}
--- src/test/scala/com/lambdarat/PasswordValidatorSpec.scala
+++ src/test/scala/com/lambdarat/PasswordValidatorSpec.scala
@@ -7,13 +7,13 @@ import org.scalacheck.Arbitrary.arbitrary
 
 class PasswordValidatorSpec extends FunSuite with ScalaCheckSuite {
 
-/*
+  /*
 Have more than 8 characters
 Contains a capital letter
 Contains a lowercase
 Contains a number
 Contains an underscore
-*/
+   */
 
   test("All passwords with more than 8 characters must be valid") {
     forAll(Generators.strAtLeastNGen(9, 100)) { password =>
@@ -35,5 +35,14 @@ Contains an underscore
     }
   }
 
+  test("All password shoud have at least one capital letter") {
+    forAll(Generators.validPasswordGen) { password =>
+      val result = PasswordValidator.validatePassword(password)
+
+      val expected = true
+
+      assertEquals(result, expected)
+    }
+  }
 
 }
