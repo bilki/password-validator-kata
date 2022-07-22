a771b07c09209bf145563990b6497e915126285c
Roberto Serrano <r.semarcos@gmail.com>
2022-07-22 17:56:59 +0200

    Green - All passwords without any capital letter must validate to false

--- src/main/scala/com/lambdarat/PasswordValidator.scala
+++ src/main/scala/com/lambdarat/PasswordValidator.scala
@@ -2,6 +2,6 @@ package com.lambdarat
 
 object PasswordValidator {
 
-  def validatePassword(password: String): Boolean = password.size > 8
+  def validatePassword(password: String): Boolean = password.size > 8 && password.exists(_.isUpper)
 
 }
