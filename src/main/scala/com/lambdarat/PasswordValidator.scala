b845dee9e55138307895da6c0a555b772dcafcbc
Roberto Serrano <r.semarcos@gmail.com>
2022-07-22 18:08:36 +0200

    Green - All passwords without any number must validate to false

--- src/main/scala/com/lambdarat/PasswordValidator.scala
+++ src/main/scala/com/lambdarat/PasswordValidator.scala
@@ -7,8 +7,10 @@ object PasswordValidator {
   val sizeRule: String => Boolean      = _.size >= MIN_PASSWORD_SIZE
   val upperCaseRule: String => Boolean = _.exists(_.isUpper)
   val lowerCaseRule: String => Boolean = _.exists(_.isLower)
+  val digitRule: String => Boolean     = _.exists(_.isDigit)
 
   def validatePassword(password: String): Boolean =
-    sizeRule(password) && upperCaseRule(password) && lowerCaseRule(password)
+    sizeRule(password) && upperCaseRule(password) &&
+      lowerCaseRule(password) && digitRule(password)
 
 }
