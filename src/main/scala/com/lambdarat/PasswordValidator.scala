2150a84b4453834c1e7b07db42c114f04750f9d6
Roberto Serrano <r.semarcos@gmail.com>
2022-07-22 18:18:32 +0200

    Green - All passwords without any underscore must validate to false

--- src/main/scala/com/lambdarat/PasswordValidator.scala
+++ src/main/scala/com/lambdarat/PasswordValidator.scala
@@ -6,16 +6,18 @@ object PasswordValidator {
 
   type Rule = String => Boolean
 
-  val sizeRule: Rule      = _.size >= MIN_PASSWORD_SIZE
-  val upperCaseRule: Rule = _.exists(_.isUpper)
-  val lowerCaseRule: Rule = _.exists(_.isLower)
-  val digitRule: Rule     = _.exists(_.isDigit)
+  val sizeRule: Rule       = _.size >= MIN_PASSWORD_SIZE
+  val upperCaseRule: Rule  = _.exists(_.isUpper)
+  val lowerCaseRule: Rule  = _.exists(_.isLower)
+  val digitRule: Rule      = _.exists(_.isDigit)
+  val underscoreRule: Rule = _.exists(_ == '_')
 
   val allRules: List[Rule] = List(
     sizeRule,
     upperCaseRule,
     lowerCaseRule,
-    digitRule
+    digitRule,
+    underscoreRule
   )
 
   def validatePassword(password: String): Boolean =
