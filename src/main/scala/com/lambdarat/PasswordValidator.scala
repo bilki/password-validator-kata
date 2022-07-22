52efe1143d54b5d653ab32994959b98a69d0217f
Roberto Serrano <r.semarcos@gmail.com>
2022-07-22 18:11:42 +0200

    Refactor to folding a list of rules

--- src/main/scala/com/lambdarat/PasswordValidator.scala
+++ src/main/scala/com/lambdarat/PasswordValidator.scala
@@ -4,13 +4,23 @@ object PasswordValidator {
 
   val MIN_PASSWORD_SIZE = 9
 
-  val sizeRule: String => Boolean      = _.size >= MIN_PASSWORD_SIZE
-  val upperCaseRule: String => Boolean = _.exists(_.isUpper)
-  val lowerCaseRule: String => Boolean = _.exists(_.isLower)
-  val digitRule: String => Boolean     = _.exists(_.isDigit)
+  type Rule = String => Boolean
+
+  val sizeRule: Rule      = _.size >= MIN_PASSWORD_SIZE
+  val upperCaseRule: Rule = _.exists(_.isUpper)
+  val lowerCaseRule: Rule = _.exists(_.isLower)
+  val digitRule: Rule     = _.exists(_.isDigit)
+
+  val allRules: List[Rule] = List(
+    sizeRule,
+    upperCaseRule,
+    lowerCaseRule,
+    digitRule
+  )
 
   def validatePassword(password: String): Boolean =
-    sizeRule(password) && upperCaseRule(password) &&
-      lowerCaseRule(password) && digitRule(password)
+    allRules.foldLeft(true) { case (isValidPassword, nextRule) =>
+      isValidPassword && nextRule(password)
+    }
 
 }
