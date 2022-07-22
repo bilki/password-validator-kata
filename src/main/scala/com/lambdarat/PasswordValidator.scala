a6a58e2dfaed69844307f9828d653d5e66397341
Roberto Serrano <r.semarcos@gmail.com>
2022-07-22 18:04:10 +0200

    Refactor password rules to functions

--- src/main/scala/com/lambdarat/PasswordValidator.scala
+++ src/main/scala/com/lambdarat/PasswordValidator.scala
@@ -2,8 +2,13 @@ package com.lambdarat
 
 object PasswordValidator {
 
+  val MIN_PASSWORD_SIZE = 9
+
+  val sizeRule: String => Boolean      = _.size >= MIN_PASSWORD_SIZE
+  val upperCaseRule: String => Boolean = _.exists(_.isUpper)
+  val lowerCaseRule: String => Boolean = _.exists(_.isLower)
+
   def validatePassword(password: String): Boolean =
-    password.size > 8 && password.exists(_.isUpper) &&
-      password.exists(_.isLower)
+    sizeRule(password) && upperCaseRule(password) && lowerCaseRule(password)
 
 }
--- src/test/scala/com/lambdarat/Generators.scala
+++ src/test/scala/com/lambdarat/Generators.scala
@@ -3,20 +3,24 @@ package com.lambdarat
 import org.scalacheck.Arbitrary
 import org.scalacheck.Gen
 
+import PasswordValidator.MIN_PASSWORD_SIZE
+
 object Generators {
 
   lazy val fewerThanEightChars =
-    Gen.listOfN(8, Arbitrary.arbChar.arbitrary).map(_.mkString)
+    Gen
+      .listOfN(MIN_PASSWORD_SIZE - 1, Arbitrary.arbChar.arbitrary)
+      .map(_.mkString)
 
   lazy val withoutCapitalLetter =
     for {
-      size   <- Gen.choose(9, 100)
+      size   <- Gen.choose(MIN_PASSWORD_SIZE, 100)
       output <- Gen.listOfN(size, Gen.alphaLowerChar)
     } yield output.mkString
 
   lazy val withoutLowerCaseLetter =
     for {
-      size   <- Gen.choose(9, 100)
+      size   <- Gen.choose(MIN_PASSWORD_SIZE, 100)
       output <- Gen.listOfN(size, Gen.alphaUpperChar)
     } yield output.mkString
 
