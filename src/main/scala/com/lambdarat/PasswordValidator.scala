aa6a1b6436bed461dd75c2b5fb0358271368e195
Roberto Serrano <r.semarcos@gmail.com>
2022-07-23 11:44:03 +0200

    Green - New validator functions should validate to false with more chars

--- src/main/scala/com/lambdarat/PasswordValidator.scala
+++ src/main/scala/com/lambdarat/PasswordValidator.scala
@@ -2,31 +2,47 @@ package com.lambdarat
 
 object PasswordValidator {
 
-  val MIN_PASSWORD_SIZE   = 8
-  val PASSWORD_UNDERSCORE = '_'
+  val MIN_PASSWORD_SIZE             = 8
+  val MIN_PASSWORD_SIZE_VALIDATION2 = 6
+  val MIN_PASSWORD_SIZE_VALIDATION3 = 16
+  val PASSWORD_UNDERSCORE           = '_'
 
   type Rule = String => Boolean
 
-  val sizeRule: Rule       = _.size > MIN_PASSWORD_SIZE
+  def sizeRuleWith(min: Int): Rule = _.size > min
+
   val upperCaseRule: Rule  = _.exists(_.isUpper)
   val lowerCaseRule: Rule  = _.exists(_.isLower)
   val digitRule: Rule      = _.exists(_.isDigit)
   val underscoreRule: Rule = _.exists(_ == PASSWORD_UNDERSCORE)
 
-  val allRules: List[Rule] = List(
-    sizeRule,
+  val validatePasswordRules: List[Rule] = List(
+    sizeRuleWith(MIN_PASSWORD_SIZE),
     upperCaseRule,
     lowerCaseRule,
     digitRule,
     underscoreRule
   )
 
-  def validatePassword(password: String): Boolean =
-    allRules.foldLeft(true) { case (isValidPassword, nextRule) =>
+  def validatePasswordWithRules(password: String, rules: List[Rule]): Boolean =
+    rules.foldLeft(true) { case (isValidPassword, nextRule) =>
       isValidPassword && nextRule(password)
     }
 
-  def validatePassword2(password: String): Boolean = ???
+  def validatePassword(password: String): Boolean =
+    validatePasswordWithRules(password, validatePasswordRules)
 
-  def validatePassword3(password: String): Boolean = ???
+  val validatePassword2Rules: List[Rule] = List(
+    sizeRuleWith(MIN_PASSWORD_SIZE_VALIDATION2)
+  )
+
+  def validatePassword2(password: String): Boolean =
+    validatePasswordWithRules(password, validatePassword2Rules)
+
+  val validatePassword3Rules: List[Rule] = List(
+    sizeRuleWith(MIN_PASSWORD_SIZE_VALIDATION3)
+  )
+
+  def validatePassword3(password: String): Boolean =
+    validatePasswordWithRules(password, validatePassword3Rules)
 }
